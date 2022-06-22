298d391269b482da398973a3e94b48b0c14538c5
onionchop <ruthhyo@gmail.com>
2022-06-21 22:28:13 -0400

    forgot to save whoopsies

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,19 @@
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== null){
-    return "![![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)]";
-  } else {
-    return "";
-  }
+ 
 }
 
 // Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== null) {
-    return `\n* [License] (license)\n`;
-  } else {
-    return ``;
-  }
+  
 }
 
 // Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== null) {
-    return `## License
-  
-    The content of this site is licensed under the ${license}
-    `;
-  }
+ 
 
 }
 
@@ -66,7 +53,8 @@ ${data.test}
 
 ## Questions
 
-Have a question? Feel free to email me at (${data.email}) or reach out at my Github (https://github.com/${data.github}):^).
+Have a question? Feel free to email me at (${data.email}) or reach out at my Github (https://github.com/${data.github})
+:^).
 
 `;
 }
